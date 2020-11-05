146f83e238e5b6f6ac453903dbb2008bc1f2b78a
xSklzx Dokkaebi <wrightricky64@gmail.com>
2020-11-05 07:42:27 +0000

    Modified assembly information.

--- DokkaebiCallouts/Properties/AssemblyInfo.cs
+++ DokkaebiCallouts/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DokkaebiCallouts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A callout modification for FivePD, a law enforcement mod for FiveM.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ZF Developments")]
 [assembly: AssemblyProduct("DokkaebiCallouts")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © xSklzx Dokkaebi 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
